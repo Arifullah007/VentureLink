b2d149105a6d56955f4f769035088a498f79b3de
SHAIK ARIFULLAH <shaikarifullah06@gmail.com>
2025-09-21 18:06:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/middleware.ts
+++ src/middleware.ts
@@ -103,8 +103,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * - / (the root page)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|^/$).*)',
   ],
 };
