2df8d026a6a96b424b70329e52fd2d246cc11e47
Bheemarasetty Harini <harinibheemarasetty5@gmail.com>
2025-09-23 18:59:22 +0000

    is it still running or done?

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // This is required to allow the Next.js dev server to accept requests from
+  // the Cloud Workstation and other development environments.
+  allowedDevOrigins: [
+      "*.cluster-52r6vzs3ujeoctkkxpjif3x34a.cloudworkstations.dev",
+      "*.cloudworkstations.dev"
+  ],
   experimental: {
-    // This is required to allow the Next.js dev server to accept requests from
-    // the Cloud Workstation and other development environments.
-    allowedDevOrigins: [
-        "*.cluster-52r6vzs3ujeoctkkxpjif3x34a.cloudworkstations.dev",
-        "*.cloudworkstations.dev"
-    ],
   },
   typescript: {
     ignoreBuildErrors: true,
