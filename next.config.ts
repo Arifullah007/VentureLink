dedd47b778f3b1c70459f7a5032eddb41297cd2a
Bheemarasetty Harini <harinibheemarasetty5@gmail.com>
2025-09-23 16:18:28 +0000

    done ?

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // This is required to allow the Next.js dev server to accept requests from
+    // the Cloud Workstation and other development environments.
+    allowedDevOrigins: [
+        "*.cluster-52r6vzs3ujeoctkkxpjif3x34a.cloudworkstations.dev",
+        "*.cloudworkstations.dev"
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
