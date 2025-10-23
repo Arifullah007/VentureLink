4cbfda7017833ba5d4c988114fb4a6667806f87e
SHAIK ARIFULLAH <shaikarifullah06@gmail.com>
2025-10-23 16:23:43 +0000

    take off everything related to hydration and why , what is didnt match w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontBody.variable, fontCode.variable)}>
         {children}
         <Toaster />
